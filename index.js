7ce398e6fac25562a67119c206e0c4d4cffcdfda
Michael Z <flashman123@fastmail.fm>
2021-08-24 15:35:24 +0100

    setup

---
+++ index.js
@@ -0,0 +1,11 @@
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log("listening");
+});
