1d279adcc898a3cdd3e1326bf76877d484d981f3
Michael Z <flashman123@fastmail.fm>
2021-08-24 16:18:48 +0100

    simple get/post added

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const { connection } = require("./db");
 const app = express();
 const port = process.env.PORT
 
+app.get("/", (req, res) => {
+    res.status(200).send("yes, i am here!");
+});
+
+app.get("/user", (req, res) => {
+    res.status(200).json({"msg": `You are viewing the user ${req.body.name}.`});
+});
+
+app.post("/user", (req, res) => {
+    res.status(201).json({"name": "created user"});
+});
+
 app.listen(port, () => {
     console.log("listening");
 });
