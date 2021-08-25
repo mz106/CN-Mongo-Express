86882cf08957758a2a35cd06244e26b18b6e810d
Michael Z <flashman123@fastmail.fm>
2021-08-25 15:00:30 +0100

    error route added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const { connection } = require("./db");
 const userRouter = require("./routes/user");
 const indexRouter = require("./routes/index");
+const errorRouter = require("./routes/error");
 
 const app = express();
 const port = process.env.PORT;
@@ -14,6 +15,7 @@ app.use(express.json()); ///remember this!!!!!!!
 
 app.use("/user", userRouter);
 app.use("/", indexRouter);
+app.use("*", errorRouter);
 
 app.listen(port, () => {
     console.log("listening");
